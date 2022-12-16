508e7bbc4ec80a1e2c80253fd960b12351342956
alejandro.alain <alejandro.alain@achievewithoutborders.com>
2022-12-16 10:22:57 +0800

    updated awb_9_box_appraisal

--- awb_9_box_appraisal/models/models.py
+++ awb_9_box_appraisal/models/models.py
@@ -9,8 +9,9 @@ class AppraisalMenu(models.Model):
     _rec_name = "name"
 
     name = fields.Char(string="Appraisal Period")
-    start_date = fields.Date(string="Start Date")
-    end_date = fields.Date(string="End Date")
+    start_date = fields.Date(string="Start Date", required=True)
+    end_date = fields.Date(string="End Date", required=True)
+    period_id = fields.Integer(string="Period Id")
 
     @api.model
     def create(self, vals):
@@ -28,4 +29,23 @@ class AppraisalMenu(models.Model):
                 'name': rec.name
             }
             period_id = self.env['period'].create(val)
+        vals['period_id'] = appraisal_period_id.id
         return super(AppraisalMenu, self).create(vals)
+
+    @api.onchange('name', 'start_date', 'end_date')
+    def _onchange_appraisal(self):
+        appraisal_period_id = self.env['appraisal.period'].search([('id', '=', self.period_id)])
+        if self.name:
+            appraisal_period_id.write({'name': self.name})
+        if self.start_date:
+            appraisal_period_id.write({'start_date': self.start_date})
+        if self.end_date:
+            appraisal_period_id.write({'end_date': self.end_date})
+
+    def unlink(self):
+        for rec in self:
+            appraisal_period_id = self.env['appraisal.period'].search(
+                [('id', '=', rec.period_id)])
+            appraisal_period_id.unlink()
+        return super(AppraisalMenu, self).unlink()
+
